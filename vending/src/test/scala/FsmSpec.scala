8f70d71ed63cf07e9032aa411bf818d39bc5e03d
Lohmann27 <s203876@student.dtu.dk>
2022-05-18 22:28:34 +0200

    FINAL TEST - DET VIRKER!!!! FsmSpec

--- vending/src/test/scala/FsmSpec.scala
+++ vending/src/test/scala/FsmSpec.scala
@@ -24,16 +24,16 @@ class FsmSpec extends AnyFlatSpec with ChiselScalatestTester {
       // Testing subPrice
       dut.io.buy.poke(true.B)
       dut.io.buyCheck.poke(true.B)
-      dut.clock.step(1)
-      dut.io.buy.poke(false.B)
-      dut.clock.step(1)
-      dut.io.buyCheck.poke(false.B)
+      dut.clock.step(2)
       dut.io.subPrice.expect(true.B)
+      dut.io.buy.poke(false.B)
+      dut.io.buyCheck.poke(false.B)
+
 
       // Testing releaseCan
       dut.clock.step(1)
       dut.io.releaseCan.expect(true.B)
-      dut.clock.step(1)
+      dut.clock.step(10)
 
       // ready
       dut.io.add2.expect(false.B)
@@ -43,29 +43,48 @@ class FsmSpec extends AnyFlatSpec with ChiselScalatestTester {
       dut.io.releaseCan.expect(false.B)
 
 
+      // Testing alarm
+      dut.io.buy.poke(true.B)
+      dut.clock.step(2)
+      dut.io.alarm.expect(true.B)
+      dut.io.buy.poke(false.B)
+      dut.clock.step(10)
+
+      // ready
+      dut.io.add2.expect(false.B)
+      dut.io.add5.expect(false.B)
+      dut.io.alarm.expect(false.B)
+      dut.io.subPrice.expect(false.B)
+      dut.io.releaseCan.expect(false.B)
+
       // Testing add2
       dut.io.coin2.poke(true.B)
       dut.clock.step(1)
       dut.io.add2.expect(true.B)
       dut.io.coin2.poke(false.B)
-      dut.clock.step(1)
-      dut.buttonCheck.expect(true.B)
-      dut.clock.step(1)
+      dut.clock.step(10)
+
+      // ready
+      dut.io.add2.expect(false.B)
+      dut.io.add5.expect(false.B)
+      dut.io.alarm.expect(false.B)
+      dut.io.subPrice.expect(false.B)
+      dut.io.releaseCan.expect(false.B)
+
 
       // Testing add5
       dut.io.coin5.poke(true.B)
       dut.clock.step(1)
       dut.io.coin5.poke(false.B)
       dut.io.add5.expect(true.B)
-      dut.clock.step(1)
+      dut.clock.step(10)
 
-      // Testing alarm
-      dut.io.buy.poke(true.B)
-      dut.io.buyCheck.poke(false.B)
-      dut.clock.step(1)
-      dut.io.alarm.expect(true.B)
-      dut.io.buy.poke(false.B)
-      dut.clock.step(1)
+      // ready
+      dut.io.add2.expect(false.B)
+      dut.io.add5.expect(false.B)
+      dut.io.alarm.expect(false.B)
+      dut.io.subPrice.expect(false.B)
+      dut.io.releaseCan.expect(false.B)
 
     }
   }
