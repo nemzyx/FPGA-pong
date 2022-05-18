59655fed7643904a4a9d7fa0ab9bd6bf701f8738
Lohmann27 <s203876@student.dtu.dk>
2022-05-18 20:32:53 +0200

    Update FsmSpec.scala

--- vending/src/test/scala/FsmSpec.scala
+++ vending/src/test/scala/FsmSpec.scala
@@ -19,6 +19,26 @@ class FsmSpec extends AnyFlatSpec with ChiselScalatestTester {
 
       // Testing each state
 
+      // Testing subPrice
+      dut.io.buy.poke(true.B)
+      dut.io.buyCheck.poke(true.B)
+      dut.clock.step(1)
+      dut.io.buy.poke(false.B)
+      dut.io.buyCheck.poke(false.B)
+      dut.io.subPrice.expect(true.B)
+
+      // Testing releaseCan
+      dut.clock.step(1)
+      dut.io.releaseCan.expect(true.B)
+      dut.clock.step(5)
+
+      // ready
+      dut.io.add2.poke(false.B)
+      dut.io.add5.poke(false.B)
+      dut.io.alarm.poke(false.B)
+      dut.io.subPrice.poke(false.B)
+      dut.io.releaseCan.poke(false.B)
+
       // Testing the ready state
       dut.io.add2.poke(false.B)
       dut.io.add5.poke(false.B)
@@ -31,7 +51,9 @@ class FsmSpec extends AnyFlatSpec with ChiselScalatestTester {
       dut.clock.step(1)
       dut.io.coin2.poke(false.B)
       dut.io.add2.expect(true.B)
-      dut.clock.step(2)
+      dut.clock.step(1)
+      dut.buttonCheck.expect(true.B)
+      dut.clock.step(1)
 
       // Testing add5
       dut.io.coin5.poke(true.B)
@@ -40,30 +62,12 @@ class FsmSpec extends AnyFlatSpec with ChiselScalatestTester {
       dut.io.add5.expect(true.B)
       dut.clock.step(2)
 
-      // Testing subPrice
-      dut.io.buy.poke(true.B)
-      dut.io.buyCheck.poke(true.B)
-      dut.clock.step(1)
-      dut.io.buy.poke(false.B)
-      dut.io.buyCheck.poke(false.B)
-      dut.io.subPrice.expect(true.B)
 
-      // Testing releaseCan
-      dut.clock.step(1)
-      dut.io.releaseCan.expect(true.B)
-      dut.clock.step(10)
-
-      // ready
-      dut.io.add2.poke(false.B)
-      dut.io.add5.poke(false.B)
-      dut.io.alarm.poke(false.B)
-      dut.io.subPrice.poke(false.B)
-      dut.io.releaseCan.poke(false.B)
 
       // Testing alarm
       dut.io.buy.poke(true.B)
       dut.io.buyCheck.poke(false.B)
-      dut.clock.step(2)
+      dut.clock.step(1)
       dut.io.alarm.expect(true.B)
       dut.io.buy.poke(false.B)
       dut.clock.step(2)
